3182ac68427664f3a7008a3fcbd78189f0183926
JunIkeda <jun.ikeda0121@gmail.com>
2022-03-08 11:01:00 +0900

    edit NavigationProvider.tsx (persist navigation state)

--- src/providers/NavigationProvider.tsx
+++ src/providers/NavigationProvider.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 
 import { NavigationContainer } from '@react-navigation/native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import { Theme } from 'src/interface';
 
@@ -9,10 +10,34 @@ interface NavigationProviderProps {
   theme: Theme;
 }
 
+const NavState_Key = 'NavState_Key';
 const NavigationProvider = (props: NavigationProviderProps) => {
   const { children, theme } = props;
+  const [isReady, setIsReady] = React.useState(false);
+  const [initialNavState, setInitialNavState] = React.useState();
+
+  React.useEffect(() => {
+    const restoreNavState = async () => {
+      try {
+        const savedNavStateString = await AsyncStorage.getItem(NavState_Key);
+        const state = savedNavStateString ? JSON.parse(savedNavStateString) : undefined;
+        if (state !== undefined) { setInitialNavState(state); }
+      } catch (e) {
+        console.error(e);
+      } finally {
+        setIsReady(true);
+      }
+    };
+    if (!isReady) { restoreNavState(); }
+  }, [isReady]);
+
+  const saveNavState = (navState) => {
+    AsyncStorage.setItem(NavState_Key, JSON.stringify(navState));
+  };
+
+  if (!isReady) { return null; }
   return (
-    <NavigationContainer theme={theme}>
+    <NavigationContainer theme={theme} initialState={initialNavState} onStateChange={saveNavState}>
       {children}
     </NavigationContainer>
   );
